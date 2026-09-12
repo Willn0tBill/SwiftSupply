61106a647bbcc0c519a2c6fffbe42888a01fa8bb
Willn0tBill <155497686+Willn0tBill@users.noreply.github.com>
2026-09-11 17:44:40 -0700

    Add SwiftSupply public configuration

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.SWIFTSUPPLY_CONFIG={
+  SUPABASE_URL:'https://phgqgdalmdtvsmqfylnl.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY:'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE',
+  FORM_ID:'mqpkpkjo',
+  ADMIN_EMAIL:'winotbill@gmail.com',
+  EMAIL_FROM:'SwiftSupply <noreply@yucai.org>'
+};
