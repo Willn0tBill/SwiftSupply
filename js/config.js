a871aa2013cc6f582af8b10d2e321cb08e2022a2
Willn0tBill <155497686+Willn0tBill@users.noreply.github.com>
2026-09-11 18:05:14 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.SWIFTSUPPLY_CONFIG={
   SUPABASE_URL:'https://phgqgdalmdtvsmqfylnl.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY:'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE',
+  SUPABASE_PUBLISHABLE_KEY:'sb_publishable_tQ1LD0sjVRowkHoiU3XHhA_Ib3KEDBZ',
   FORM_ID:'mqpkpkjo',
   ADMIN_EMAIL:'winotbill@gmail.com',
   EMAIL_FROM:'SwiftSupply <noreply@yucai.org>'
